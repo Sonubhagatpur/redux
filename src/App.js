d5b2234fe6f7e6455a2ee473aad24a4d2c398774
Sonu <sonusinghbhagatpur@gmail.com>
2023-12-24 01:14:42 +0530

    installation setup completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename='/'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<h2>All Products Details</h2>}></Route>
+        <Route path='/add' element={<h2>Add Products</h2>}></Route>
+        <Route path='/update' element={<h2>Update Products Details</h2>}></Route>
+        <Route path='/profile' element={<h2>This is my profile page</h2>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
